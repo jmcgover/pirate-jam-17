d9b1b74a9a2bc2e91d1d485856d40bf155907e2f
Jeffrey McGovern <jeff@jdmcg.org>
2025-07-31 03:46:13 -0700

    listen to and use input to move around

--- script/sprite_2d.gd
+++ script/sprite_2d.gd
@@ -7,6 +7,15 @@ func _init():
 	print("Hello, World!")
 
 func _process(delta):
-	rotation += angular_speed * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		
+	rotation += angular_speed * delta * direction
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
