e6e53797b4fe9278339055d2c33e5246d38dcbe9
Jeffrey McGovern <jeff@jdmcg.org>
2025-07-31 04:05:24 -0700

    toggle motion

--- script/sprite_2d.gd
+++ script/sprite_2d.gd
@@ -7,15 +7,22 @@ func _init():
 	print("Hello, World!")
 
 func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-		
-	rotation += angular_speed * delta * direction
-	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+	#var direction = 0
+	#if Input.is_action_pressed("ui_left"):
+		#direction = -1
+	#if Input.is_action_pressed("ui_right"):
+		#direction = 1
+		#
+	#rotation += angular_speed * delta * direction
+	#
+	#var velocity = Vector2.ZERO
+	#if Input.is_action_pressed("ui_up"):
+		#velocity = Vector2.UP.rotated(rotation) * speed
+	#position += velocity * delta
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing())
