c0e953119744b614ee046016037579f71029c767
Jeffrey McGovern <jeff@jdmcg.org>
2025-07-31 04:27:18 -0700

    this is the one I like
    done with this one: https://docs.godotengine.org/en/stable/getting_started/step_by_step/signals.html

--- script/sprite_2d.gd
+++ script/sprite_2d.gd
@@ -11,34 +11,18 @@ func _init():
 	print("Hello, World!")
 
 func _process(delta):
-	#var direction = 0
-	#if Input.is_action_pressed("ui_left"):
-		#direction = -1
-	#if Input.is_action_pressed("ui_right"):
-		#direction = 1
-		#
-	#rotation += angular_speed * delta * direction
-	#
-	#var velocity = Vector2.ZERO
-	#if Input.is_action_pressed("ui_up"):
-		#velocity = Vector2.UP.rotated(rotation) * speed
-	#position += velocity * delta
-	rotation += angular_speed * delta
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		
+	rotation += angular_speed * delta * direction
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
 
-func _ready():
-	var timer = get_node("Timer")
-	timer.timeout.connect(_on_timer_timeout)
-
 func _on_button_pressed() -> void:
-	set_process(not is_processing())
-
-func _on_timer_timeout():
 	visible = not visible
-
-
-func take_damage(amount):
-	var old_health = health
-	health -= amount
-	health_changed.emit(old_health, health)
